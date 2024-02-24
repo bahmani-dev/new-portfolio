474ac7ba4ef9742e363cefad503c46c54c396a22
mandegar20 <mohammadmandegar2019@gmail.com>
2024-02-24 21:31:38 +0430

    add image and titles

--- src/app/layout/header.tsx
+++ src/app/layout/header.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Image from "next/image";
 
 const  Header = () =>{
   return (
-    <h1>Header</h1>
+<header>
+  <div className={`flex justify-between h-20 `}>
+    <div className='flex gap-4 items-center px-5 '>
+      <Image src='/images/logo.svg' alt='logo image'      width={20}
+      height={20}></Image> 
+      <p className='font-bold '>Personal</p>
+    </div>
+    <div>
+ 
+    </div>
+  </div>
+
+</header>
   );
 }
  
