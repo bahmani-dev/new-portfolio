aa9e21ed72497281b7ffeb1da539b5a9a89bf727
mandegar20 <mohammadmandegar2019@gmail.com>
2024-02-24 21:32:35 +0430

    add container for header and hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
+import React from 'react';
 import Hero from "../component/hero";
 import Header from "./layout/header";
 
 
 export default function Home() {
   return (
-    <>
+    <div className="container">
     <Header/>
     <Hero/>
-    <p>Hello</p>
-    </>
+    </div>
   );
 }
  
