4cf4363fdc9027aec61d16666dd9e967d62f2173
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-05 22:44:15 +0430

    add  img and txt

--- src/component/hero.tsx
+++ src/component/hero.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 const Hero = () => {
   return (
-    <div>hero</div>
-  )
-}
+    <div className="px-5  flex flex-col gap-10 ">
+      <div className="flex items-center justify-center">
+        <Image
+          src="/images/Boy/boy.png"
+          alt="alt"
+          width={300}
+          height={300}
+          layout="responsive"
+        />
+      </div>
+      <h1>
+        Hello I&apos;m <span className="font-bold">Mohammad Bahmani</span>
+      </h1>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
